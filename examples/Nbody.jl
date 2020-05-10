69a3aa2db0c5f9a4d712cc25da1e283af7fd41a9
Joseba <mikel.antonana@gmail.com>
2020-05-10 12:40:46 +0200

    solar-system15

--- examples/Nbody.jl
+++ examples/Nbody.jl
@@ -4,12 +4,32 @@ function NbodyEnergy(u,Gm)
      T = zerouel
      U = zerouel
      for i in 1:N
-        qi = u[2,:,i]   #  x = view(u,1:7)   # x
+        qi = u[2,:,i]
         vi = u[1,:,i]
         Gmi = Gm[i]
         T += Gmi*(vi[1]*vi[1]+vi[2]*vi[2]+vi[3]*vi[3])
         for j in (i+1):N
-           qj = u[2,:,j]
+           qj = u[2,:,j]  # qj = u[2,:,j]
+           Gmj = Gm[j]
+           qij = qi - qj
+           U -= Gmi*Gmj/norm(qij)
+        end
+     end
+    1/2*T + U
+end
+
+function NbodyEnergy2(u,Gm)
+     N = length(Gm)
+     zerouel = zero(eltype(u))
+     T = zerouel
+     U = zerouel
+     for i in 1:N
+        qi = u[1,:,i]    # qi = u[2,:,i]
+        vi = u[2,:,i]    # vi = u[1,:,i]
+        Gmi = Gm[i]
+        T += Gmi*(vi[1]*vi[1]+vi[2]*vi[2]+vi[3]*vi[3])
+        for j in (i+1):N
+           qj = u[1,:,j]  # qj = u[2,:,j]
            Gmj = Gm[j]
            qij = qi - qj
            U -= Gmi*Gmj/norm(qij)
@@ -19,23 +39,91 @@ function NbodyEnergy(u,Gm)
 end
 
 
+# OdeProblem
+
 function NbodyODE!(du,u,Gm,t)
      N = length(Gm)
-     du[1,:,:] .= 0
+     du[1,:,:] .= 0    # du[1,:,:] .= 0
      for i in 1:N
-        qi = u[2,:,i]
+        qi = u[2,:,i]          # qi = u[2,:,i]
         Gmi = Gm[i]
-        du[2,:,i] = u[1,:,i]
+        du[2,:,i] = u[1,:,i]   # du[2,:,i] = u[1,:,i]
         for j in (i+1):N
-           qj = u[2,:,j]
+           qj = u[2,:,j]       # qj = u[2,:,j]
            Gmj = Gm[j]
            qij = qi - qj
            auxij = (qij[1]*qij[1]+qij[2]*qij[2]+qij[3]*qij[3])^(-3/2)
-           du[1,:,i] -= Gmj*auxij*qij
-           du[1,:,j] += Gmi*auxij*qij
+           du[1,:,i] -= Gmj*auxij*qij    # du[1,:,i] -= Gmj*auxij*qij
+           du[1,:,j] += Gmi*auxij*qij    # du[1,:,j] += Gmi*auxij*qij
         end
      end
 
     return
-    
+
+end
+
+
+# DynamicalODEProblem
+
+
+function NbodyODEv!(dv,q,v,Gm,t)
+#
+#    dotv
+#
+     N = length(Gm)
+     dv[:,:] .= 0
+     for i in 1:N
+        qi = q[:,i]
+        Gmi = Gm[i]
+        for j in (i+1):N
+           qj = q[:,j]
+           Gmj = Gm[j]
+           qij = qi - qj
+           auxij = (qij[1]*qij[1]+qij[2]*qij[2]+qij[3]*qij[3])^(-3/2)
+           dv[:,i] -= Gmj*auxij*qij
+           dv[:,j] += Gmi*auxij*qij
+        end
+     end
+
+    return
+
+end
+
+
+
+function NbodyODEq!(dq,q,v,Gm,t)
+#
+#    dotq
+#
+     N = length(Gm)
+     for i in 1:N
+        dq[:,i] = v[:,i]
+     end
+
+    return
+
+end
+
+
+## 2ndOrderProblem
+
+
+function NbodyODE2nd!(ddu,du,u,Gm,t)
+     N = length(Gm)
+     ddu[:,:] .= 0
+     for i in 1:N
+        qi = u[:,i]
+        Gmi = Gm[i]
+        for j in (i+1):N
+           qj = u[:,j]
+           Gmj = Gm[j]
+           qij = qi - qj
+           auxij = (qij[1]*qij[1]+qij[2]*qij[2]+qij[3]*qij[3])^(-3/2)
+           ddu[:,i] -= Gmj*auxij*qij
+           ddu[:,j] += Gmi*auxij*qij
+        end
+     end
+
+    return
+
 end
